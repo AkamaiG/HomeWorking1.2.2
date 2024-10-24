723b14baf5c55454e95f957ca9d9185e8412b4ff
AkamaiG <kir2956@yandex.ru>
2024-10-25 00:50:54 +0300

    complete work 1.5.4

--- HomeWorking/HomeWorking.cpp
+++ HomeWorking/HomeWorking.cpp
@@ -6,50 +6,33 @@ int main()
     /* Установка кодировки */
     setlocale(LC_ALL, "rus");
     
-    /* Инициализация переменных массива */
-    const int rows = 3;
-    const int cols = 6;
-    int array[rows][cols] = {
-        {5, 2, 8, 1, 6, 4},
-        {9, 0, 7, 3, 12, 11},
-        {14, 10, 15, 18, 17, 16}
-    };
+    /* Инициализация массива целых чисел */
+    int arr[10] = { 34, 12, 5, 67, 23, 90, 1, 45, 28, 76 };
 
-    /* Выводим массив */
-    std::cout << "Массив:" << std::endl;
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            std::cout << array[i][j] << "\t";
-        }
-        std::cout << std::endl;
+    /* Вывод массива до сортировки */
+    std::cout << "Массив до сортировки: ";
+    for (int i = 0; i < 10; i++) {
+        std::cout << arr[i] << " ";
     }
+    std::cout << std::endl;
 
-    /* Инициализация переменных для минимального и максимального значения */
-    int minIndex[2] = { 0, 0 };
-    int maxIndex[2] = { 0, 0 };
-    int minValue = array[0][0];
-    int maxValue = array[0][0];
-
-    /* Находим минимальное и максимальное значения и их индексы */
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            if (array[i][j] < minValue) {
-                minValue = array[i][j];
-                minIndex[0] = i;
-                minIndex[1] = j;
-            }
-            if (array[i][j] > maxValue) {
-                maxValue = array[i][j];
-                maxIndex[0] = i;
-                maxIndex[1] = j;
+    /* Сортировка массива пузырьком в обратном порядке */
+    for (int i = 0; i < 10 - 1; i++) {
+        for (int j = 0; j < 10 - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 
-    /* Выводим индексы минимального и максимального элемента */
-    std::cout << "Индекс минимального элемента: " << minIndex[0] << " " << minIndex[1] << std::endl;
-    std::cout << "Индекс максимального элемента: " << maxIndex[0] << " " << maxIndex[1] << std::endl;
-
+    /* Вывод массива после сортировки */
+    std::cout << "Массив после сортировки: ";
+    for (int i = 0; i < 10; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
 
     return EXIT_SUCCESS;
 }
