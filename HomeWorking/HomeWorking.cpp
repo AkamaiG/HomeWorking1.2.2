1311a49763724d8a91da051d9ebf8e0861201f48
AkamaiG <kir2956@yandex.ru>
2024-10-24 23:57:43 +0300

    Revert "complete work 1.5.2"
    This reverts commit a6aabbd99fd6c98c57000896e0cfe72bf7a80348.

--- HomeWorking/HomeWorking.cpp
+++ HomeWorking/HomeWorking.cpp
@@ -11,6 +11,7 @@ int main()
 
     int minNumber = array[0];
     int maxNumber = array[0];
+
     std::string numbers;
 
     /* Узнаем длину массива для цикла */
@@ -24,6 +25,7 @@ int main()
     /* Выводим содержимое строки массива */
     std::cout << "Массив: " << numbers << std::endl;
 
+    /* Перебираем циклом массив для определения минимального и максимального значения из массива */
     for (int number : array) {
         if (number < minNumber) {
             minNumber = number;
@@ -34,6 +36,7 @@ int main()
         }
     }
 
+    /* Выводим переменные min и max */
     std::cout << "Минимальный элемент: " << minNumber << std::endl;
     std::cout << "Максимальный элемент: " << maxNumber << std::endl;
 
