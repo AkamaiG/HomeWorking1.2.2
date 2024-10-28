4e67d8f818b73cc3c837afdd862f91b53f7e05da
AkamaiG <kir2956@yandex.ru>
2024-10-28 20:47:03 +0300

    complete work 1.6.2

--- HomeWorking/HomeWorking.cpp
+++ HomeWorking/HomeWorking.cpp
@@ -1,35 +1,22 @@
 ﻿#include <iostream>
-#include <string>
 
-int sum(int a, int b) {
-	return a + b;
-}
-
-int diff(int a, int b) {
-	return a - b;
-}
-
-int multiplication(int a, int b) {
-	return a * b;
-}
-
-double division(int a, int b) {
-	return static_cast<double>(a) / b;
-}
-
-int main()
+void calc_func(int value, int power, int result)
 {
-	int a = 5, b = 10;
+	for (int i = 0; i < power; i++)
+	{
+		result *= value;
+	}
 
-	int s = sum(a, b);
-	int dif = diff(a, b);
-	int mult = multiplication(a, b);
-	double div = division(a, b);
+	std::cout << value << " в степени " << power << " = " << result << std::endl;
+}
 
-	std::cout << a << " + " << b << " = " << s << std::endl;
-	std::cout << a << " - " << b << " = " << dif << std::endl;
-	std::cout << a << " * " << b << " = " << mult << std::endl;
-	std::cout << a << " / " << b << " = " << div << std::endl;
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "rus");
+
+	calc_func(5, 2, 1);
+	calc_func(3, 3, 1);
+	calc_func(4, 4, 1);
 
 	return EXIT_SUCCESS;
 }
