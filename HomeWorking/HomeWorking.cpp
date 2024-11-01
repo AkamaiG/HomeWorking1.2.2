2b5e0eabfeeb18f2d64bf34a10c528e2b5f4ee55
AkamaiG <kir2956@yandex.ru>
2024-11-01 20:35:27 +0300

    complete work 1.7.1

--- HomeWorking/HomeWorking.cpp
+++ HomeWorking/HomeWorking.cpp
@@ -2,33 +2,25 @@
 #include <iostream>
 #include <string>
 
-int fibonacci(int n) {
-    if (n == 0) {
-        return 0;
-    }
-    else if (n == 1) {
-        return 1;
-    }
-    else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-}
-
 int main(int argc, char** argv) {
-    setlocale(LC_ALL, "rus");
+    
+    int valueInt = 10;
+    short valueShort = 10;
+    long valueLong = 10;
+    long long valueLongLong = 10;
+    float valueFloat = 10.0;
+    double valueDouble = 10.0;
+    long double valueLongDouble = 10.0;
+    bool valueBool = true;
 
-    int count{ 0 };
-
-    std::cout << "Введите число: ";
-    std::cin >> count;
-
-    std::cout << "Числа Фибоначчи: ";
-
-    for (int i = 0; i < count; ++i) {
-        std::cout << fibonacci(i) << " ";
-    }
-
-    std::cout << std::endl;
+    std::cout << "short: " << &valueShort << " " << sizeof(valueShort) << std::endl;
+    std::cout << "int: " << &valueInt << " " << sizeof(valueInt) << std::endl;
+    std::cout << "long: " << &valueLong << " " << sizeof(valueLong) << std::endl;
+    std::cout << "long long: " << &valueLongLong << " " << sizeof(valueLongLong) << std::endl;
+    std::cout << "float: " << &valueFloat << " " << sizeof(valueFloat) << std::endl;
+    std::cout << "double: " << &valueDouble << " " << sizeof(valueDouble) << std::endl;
+    std::cout << "long double: " << &valueLongDouble << " " << sizeof(valueLongDouble) << std::endl;
+    std::cout << "bool: " << &valueBool << " " << sizeof(valueBool) << std::endl;
 
     return EXIT_SUCCESS;
 }
