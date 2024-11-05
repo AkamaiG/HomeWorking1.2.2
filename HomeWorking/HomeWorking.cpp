9a89ebe538f69d26562f49ef01c22160169a4b08
AkamaiG <kir2956@yandex.ru>
2024-11-05 20:10:06 +0300

    complete work 1.8.2

--- HomeWorking/HomeWorking.cpp
+++ HomeWorking/HomeWorking.cpp
@@ -2,20 +2,52 @@
 #include <iostream>
 #include <string>
 
-void counting_function()
+namespace Math
 {
-    static int count{};
-	std::cout << "Количество вызовов функции counting_function(): " << ++count << std::endl;
+    namespace Addition
+    {
+		int calc(int a, int b)
+		{
+			return a + b;
+		}
+    }
+
+    namespace Subtraction
+    {
+        int calc(int a, int b)
+		{
+			return a - b;
+		}
+    }
+
+    namespace Multiplication
+	{
+		int calc(int a, int b)
+		{
+			return a * b;
+		}
+	}
+
+	namespace Division
+	{
+		int calc(int a, int b)
+		{
+			return a / b;
+		}
+	}
 }
 
 int main(int argc, char** argv)
 {
 	setlocale(LC_ALL, "ru");
 
-    for (int i = 0; i < 15; i++)
-    {
-        counting_function();
-    }
+	int x = 5, y = 10;
+
+	std::cout << "x = " << x << ", y = " << y << std::endl;
+	std::cout << "Сложение: " << Math::Addition::calc(x, y) << std::endl;
+	std::cout << "Вычитание: " << Math::Subtraction::calc(x, y) << std::endl;
+	std::cout << "Умножение: " << Math::Multiplication::calc(x, y) << std::endl;
+	std::cout << "Деление: " << Math::Division::calc(x, y) << std::endl;
 
     return EXIT_SUCCESS;
 }
