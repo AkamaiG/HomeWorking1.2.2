312a48fc3eaa624b66427c977ac68a3229620022
AkamaiG <kir2956@yandex.ru>
2024-11-08 17:52:53 +0300

    complete work 1.9.2

--- HomeWorking/HomeWorking.cpp
+++ HomeWorking/HomeWorking.cpp
@@ -1,22 +1,20 @@
 ﻿#include <iostream>
 
-void print(const int* array, size_t size) {
-    for (size_t i = 0; i < size; ++i) {
-        std::cout << array[i] << " ";
-    }
-    std::cout << std::endl;
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int main(int argc, char** argv) {
-    int array1[] = { 1, 2, 3, 4, 5 };
-    int array2[] = { 10, 20, 30 };
-    int array3[] = { 7, 8, 9, 10, 11, 12, 13 };
+    int a = 5;
+    int b = 10;
 
-    print(array1, sizeof(array1) / sizeof(array1[0]));
+    std::cout << "a = " << a << ", b = " << b << std::endl;
 
-    print(array2, sizeof(array2) / sizeof(array2[0]));
+    swap(&a, &b);
 
-    print(array3, sizeof(array3) / sizeof(array3[0]));
+    std::cout << "a = " << a << ", b = " << b << std::endl;
 
     return EXIT_SUCCESS;
 }
