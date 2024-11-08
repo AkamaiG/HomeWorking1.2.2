5cc42b3393ed16b270f24bb91dce46ffb132f3ca
AkamaiG <kir2956@yandex.ru>
2024-11-08 17:50:37 +0300

    complete work 1.9.1

--- HomeWorking/HomeWorking.cpp
+++ HomeWorking/HomeWorking.cpp
@@ -1,53 +1,22 @@
-﻿
-#include <iostream>
-#include <string>
+﻿#include <iostream>
 
-namespace Math
-{
-    namespace Addition
-    {
-		int calc(int a, int b)
-		{
-			return a + b;
-		}
+void print(const int* array, size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        std::cout << array[i] << " ";
     }
-
-    namespace Subtraction
-    {
-        int calc(int a, int b)
-		{
-			return a - b;
-		}
-    }
-
-    namespace Multiplication
-	{
-		int calc(int a, int b)
-		{
-			return a * b;
-		}
-	}
-
-	namespace Division
-	{
-		int calc(int a, int b)
-		{
-			return a / b;
-		}
-	}
+    std::cout << std::endl;
 }
 
-int main(int argc, char** argv)
-{
-	setlocale(LC_ALL, "ru");
+int main(int argc, char** argv) {
+    int array1[] = { 1, 2, 3, 4, 5 };
+    int array2[] = { 10, 20, 30 };
+    int array3[] = { 7, 8, 9, 10, 11, 12, 13 };
 
-	int x = 5, y = 10;
+    print(array1, sizeof(array1) / sizeof(array1[0]));
 
-	std::cout << "x = " << x << ", y = " << y << std::endl;
-	std::cout << "Сложение: " << Math::Addition::calc(x, y) << std::endl;
-	std::cout << "Вычитание: " << Math::Subtraction::calc(x, y) << std::endl;
-	std::cout << "Умножение: " << Math::Multiplication::calc(x, y) << std::endl;
-	std::cout << "Деление: " << Math::Division::calc(x, y) << std::endl;
+    print(array2, sizeof(array2) / sizeof(array2[0]));
+
+    print(array3, sizeof(array3) / sizeof(array3[0]));
 
     return EXIT_SUCCESS;
 }
