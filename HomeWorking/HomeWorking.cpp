48cf596e29a76b98d8609ade7f235de29cb0155d
AkamaiG <kir2956@yandex.ru>
2024-11-11 19:48:47 +0300

    complete work 1.10.3

--- HomeWorking/HomeWorking.cpp
+++ HomeWorking/HomeWorking.cpp
@@ -1,33 +1,55 @@
 ﻿#include <iostream>
 #include <string>
 
-double* create_array(int size) {
-    double* array = new double[size];
-
-    for (int i = 0; i < size; ++i) {
-        array[i] = 0.0;
+int** create_two_dim_array(int rows, int cols) {
+    int** array = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        array[i] = new int[cols];
     }
-
     return array;
+}
+
+void fill_two_dim_array(int** array, int rows, int cols) {
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= cols; j++) {
+            array[i - 1][j - 1] = i * j;
+        }
+    }
+}
+
+void print_two_dim_array(int** array, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            std::cout << array[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void delete_two_dim_array(int** array, int rows) {
+    for (int i = 0; i < rows; i++) {
+        delete[] array[i];
+    }
+    delete[] array;
 }
 
 int main(int argc, char** argv) {
     setlocale(LC_ALL, "RUS");
 
-    int size;
+    int rows, cols;
 
-    std::cout << "Введите размер массива: ";
-    std::cin >> size;
+    std::cout << "Введите количество строк: ";
+    std::cin >> rows;
+    std::cout << "Введите количество столбцов: ";
+    std::cin >> cols;
 
-    double* myArray = create_array(size);
+    int** multiplication_table = create_two_dim_array(rows, cols);
+    fill_two_dim_array(multiplication_table, rows, cols);
 
-    std::cout << "Массив:" << std::endl;
-    for (int i = 0; i < size; ++i) {
-        std::cout << myArray[i] << " ";
-    }
-    std::cout << std::endl;
+    std::cout << "Таблица умножения:" << std::endl;
+    print_two_dim_array(multiplication_table, rows, cols);
 
-    delete[] myArray;
+    delete_two_dim_array(multiplication_table, rows);
 
     return EXIT_SUCCESS;
 }
