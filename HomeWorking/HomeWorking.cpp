a8a19f81ed3db59214fbd1e5981c66767c089c91
AkamaiG <kir2956@yandex.ru>
2024-11-14 18:22:22 +0300

    complete work 1.11.1

--- HomeWorking/HomeWorking.cpp
+++ HomeWorking/HomeWorking.cpp
@@ -1,55 +1,22 @@
 ﻿#include <iostream>
 #include <string>
 
-int** create_two_dim_array(int rows, int cols) {
-    int** array = new int* [rows];
-    for (int i = 0; i < rows; i++) {
-        array[i] = new int[cols];
-    }
-    return array;
-}
-
-void fill_two_dim_array(int** array, int rows, int cols) {
-    for (int i = 1; i <= rows; i++) {
-        for (int j = 1; j <= cols; j++) {
-            array[i - 1][j - 1] = i * j;
-        }
-    }
-}
-
-void print_two_dim_array(int** array, int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            std::cout << array[i][j] << "\t";
-        }
-        std::cout << std::endl;
-    }
-}
-
-void delete_two_dim_array(int** array, int rows) {
-    for (int i = 0; i < rows; i++) {
-        delete[] array[i];
-    }
-    delete[] array;
-}
-
 int main(int argc, char** argv) {
     setlocale(LC_ALL, "RUS");
 
-    int rows, cols;
+    std::string firstName;
+    std::string lastName;
 
-    std::cout << "Введите количество строк: ";
-    std::cin >> rows;
-    std::cout << "Введите количество столбцов: ";
-    std::cin >> cols;
+    // Запрос имени у пользователя
+    std::cout << "Введите имя: ";
+    std::getline(std::cin, firstName);
 
-    int** multiplication_table = create_two_dim_array(rows, cols);
-    fill_two_dim_array(multiplication_table, rows, cols);
+    std::cout << "Введите фамилию: ";
+    std::getline(std::cin, lastName);
 
-    std::cout << "Таблица умножения:" << std::endl;
-    print_two_dim_array(multiplication_table, rows, cols);
+    std::string fullName = firstName + " " + lastName;
 
-    delete_two_dim_array(multiplication_table, rows);
+    std::cout << "Здравствуйте, " << fullName << "!" << std::endl;
 
     return EXIT_SUCCESS;
 }
