665ea4a03a41bcff5641456887f9287e7801dc37
AkamaiG <kir2956@yandex.ru>
2024-11-18 19:42:37 +0300

    complete work 1.12.1

--- HomeWorking/HomeWorking.cpp
+++ HomeWorking/HomeWorking.cpp
@@ -1,24 +1,22 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <string>
 
 int main(int argc, char** argv) {
-    setlocale(LC_ALL, "RUS");
+    setlocale(LC_ALL, "rus");
 
-    std::string secretWord = "netology";
-    std::string userInput;
+    std::ifstream file("in.txt");
 
-    std::cout << "Угадайте слово: ";
-
-    while (true) {
-        std::getline(std::cin, userInput);
-        if (userInput == secretWord) {
-            std::cout << "Правильно! Вы победили! Загаданное слово — " << secretWord << std::endl;
-            break;
-        }
-        else {
-            std::cout << "Неправильно\nУгадайте слово: ";
-        }
+    if (!file.is_open()) {
+        std::cerr << "Ошибка при открытии файла." << std::endl;
+        return 1;
     }
 
+    std::string word;
+    while (file >> word) {
+        std::cout << word << std::endl;
+    }
+
+    file.close();
     return EXIT_SUCCESS;
 }
