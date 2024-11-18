408792148e366e0bbbf9424356ecf5fea0769943
AkamaiG <kir2956@yandex.ru>
2024-11-18 19:46:09 +0300

    Revert "complete work 1.12.2"
    This reverts commit 3ccc58c23ed91f6928510d5cab35ac617fb94178.

--- HomeWorking/HomeWorking.cpp
+++ HomeWorking/HomeWorking.cpp
@@ -1,32 +1,22 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <string>
 
 int main(int argc, char** argv) {
     setlocale(LC_ALL, "rus");
 
-    std::ifstream inputFile("in.txt");
-    if (!inputFile) {
-        std::cerr << "Не удалось открыть файл!" << std::endl;
-        return EXIT_FAILURE;
+    std::ifstream file("in.txt");
+
+    if (!file.is_open()) {
+        std::cerr << "Ошибка при открытии файла." << std::endl;
+        return 1;
     }
 
-    int n;
-    inputFile >> n;
-
-    int arr[100];
-
-    // Считываем массив из файла
-    for (int i = 0; i < n; ++i) {
-        inputFile >> arr[i];
+    std::string word;
+    while (file >> word) {
+        std::cout << word << std::endl;
     }
 
-    inputFile.close();
-
-    for (int i = n - 1; i >= 0; --i) {
-        std::cout << arr[i] << " ";
-    }
-
-    std::cout << std::endl;
-
+    file.close();
     return EXIT_SUCCESS;
 }
