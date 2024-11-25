ed1f520be26eebd668a6a78f081108d7b7f17c9c
AkamaiG <kir2956@yandex.ru>
2024-11-25 19:19:09 +0300

    complete cpp diplom

--- HomeWorking/HomeWorking.cpp
+++ HomeWorking/HomeWorking.cpp
@@ -1,33 +1,127 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <string>
+#include <windows.h>
+#include <cstdlib>
 
-int main(int argc, char** argv) {
-    setlocale(LC_ALL, "Rus");
+using namespace std;
 
-    std::ifstream inputFile("in.txt");
+void printUniverse(int rows, int cols, char universe[][30], int generation, int aliveCount) {
+    system("cls");
+    cout << "Generation: " << generation << ". Alive cells: " << aliveCount << endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << universe[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int countAliveNeighbors(int rows, int cols, char universe[][30], int x, int y) {
+    int aliveCount = 0;
+    for (int i = -1; i <= 1; ++i) {
+        for (int j = -1; j <= 1; ++j) {
+            if (i == 0 && j == 0) continue;
+            int newX = x + i, newY = y + j;
+            if (newX >= 0 && newX < rows && newY >= 0 && newY < cols) {
+                if (universe[newX][newY] == '*') {
+                    aliveCount++;
+                }
+            }
+        }
+    }
+    return aliveCount;
+}
+
+int main() {
+    int rows, cols;
+    char universe[20][30] = { '-' };
+
+    ifstream inputFile("input.txt");
     if (!inputFile) {
-        std::cerr << "Ошибка открытия файла." << std::endl;
+        cout << "Error opening file!" << endl;
         return EXIT_FAILURE;
     }
 
-    int rows, cols;
     inputFile >> rows >> cols;
 
-    int array[50][50];
+    int x, y;
+    while (inputFile >> x >> y) {
+        universe[x][y] = '*';
+    }
+    inputFile.close();
+
+    int generation = 0;
+    int aliveCount = 0;
 
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            inputFile >> array[i][j];
+            if (universe[i][j] == '*') {
+                aliveCount++;
+            }
         }
     }
 
-    inputFile.close();
+    while (aliveCount > 0) {
+        char newUniverse[20][30] = { '-' };
+        int newAliveCount = 0;
 
-    for (int i = 0; i < rows; ++i) {
-        for (int j = cols - 1; j >= 0; --j) {
-            std::cout << array[i][j] << " ";
+        if (generation != 0) {
+            printUniverse(rows, cols, universe, generation, aliveCount);
+            Sleep(1000);
+		}
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                int neighbors = countAliveNeighbors(rows, cols, universe, i, j);
+                if (universe[i][j] == '*' && (neighbors == 2 || neighbors == 3)) {
+                    newUniverse[i][j] = '*';
+                }
+                else if (universe[i][j] == '-' && neighbors == 3) {
+                    newUniverse[i][j] = '*';
+                }
+                else {
+                    newUniverse[i][j] = '-';
+                }
+
+                if (newUniverse[i][j] == '*') {
+                    newAliveCount++;
+                }
+            }
         }
-        std::cout << std::endl;
+
+        bool stable = true;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (universe[i][j] != newUniverse[i][j]) {
+                    stable = false;
+                    break;
+                }
+            }
+            if (!stable) break;
+        }
+
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                universe[i][j] = newUniverse[i][j];
+            }
+        }
+
+        if (stable) {
+            printUniverse(rows, cols, universe, generation, aliveCount);
+            cout << "Game over: stable configuration reached." << endl;
+            break;
+        }
+
+        generation++;
+        aliveCount = newAliveCount;
+    }
+
+    if (aliveCount == 0) {
+        printUniverse(rows, cols, universe, generation, aliveCount);
+        cout << "Game over: no living cells." << endl;
     }
 
     return EXIT_SUCCESS;
